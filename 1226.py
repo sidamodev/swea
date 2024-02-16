c29e45cd849b57af24409f1a160f7c0380effe43
devchoi <dev.sidamo@gmail.com>
2024-02-16 22:15:49 +0900

    aa

--- 1226.py
+++ 1226.py
@@ -1,16 +1,14 @@
-import sys
-sys.stdin = open("1226.txt", "r")
-
 def dfs(i, j):
     maze[i][j] = 1
     for di, dj in d_ij:
         if maze[i + di][j + dj] != 1:
             dfs(i + di, j + dj)
 
+
 for test_case in range(1, 11):
     tc = int(input())
     maze = [list(map(int, list(input()))) for _ in range(16)]
     d_ij = ((-1, 0), (1, 0), (0, -1), (0, 1))
+    arr = [(i, j) for j in range(16) for i in range(16) if maze[i][j] == 3][0]
     dfs(1, 1)
     print(f'#{tc} {1 if maze[arr[0]][arr[1]] == 1 else 0}')
-    # pprint.pprint(maze)
--- 4880.py
+++ 4880.py
@@ -4,26 +4,28 @@ sys.stdin = open('4880.txt', 'r')
 
 
 def solve(start, end):
-    winner = 0
+    mid = (start + end) // 2
     if end == start:
         return start
-    mid = (start + end) // 2
-    result = 0
     winner1 = solve(start, mid)
     winner2 = solve(mid + 1, end)
-    if card_list[winner1] == 1:
-        if card_list[winner2] == 3:
-            winner = card_list[winner1]
+    result = winner1
+    if card_list[winner1] > card_list[winner2]:
+        if card_list[winner1] == 3 and card_list[winner2] == 1:
+            result = winner2
+        else:
+            result = winner1
     elif card_list[winner1] < card_list[winner2]:
-        winner = card_list[winner2]
-    elif card_list[winner1] > card_list[winner2]:
-        if card_list[winner2] != 1:
-            winner = winner1
-    return winner
-
+        if card_list[winner1] == 1 and card_list[winner2] == 3:
+            result = winner1
+        else:
+            result = winner2
+    elif card_list[winner1] == card_list[winner2]:
+        result = winner1 if winner1 < winner2 else winner2
+    return result
 
 T = int(input())
 for test_case in range(1, T + 1):
     N = int(input())
     card_list = list(map(int, input().split()))
-    print(solve(0, N))
+    print(f'#{test_case} {solve(0, N - 1) + 1}')
