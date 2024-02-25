d358fa37bf12f50e83df3ad5634dc831dad73e33
devchoi <dev.sidamo@gmail.com>
2024-02-25 11:54:42 +0900

    fix: solved 1242

--- 1242.py
+++ 1242.py
@@ -1,93 +1,73 @@
 import sys
-import pprint
 
-sys.stdin = open('1242.txt', 'r')
+sys.stdin = open("1242.txt", "r")
 T = int(input())
-
-
-def find_start(start):
-    for i in range(start, N):
-        for char in code[i]:
-            if char != '0' and char in hex_dict.keys():
-                return i
-
-
-hex_dict = {
-    '0': "0000",
-    '1': "0001",
-    '2': "0010",
-    '3': "0011",
-    '4': "0100",
-    '5': "0101",
-    '6': "0110",
-    '7': "0111",
-    '8': "1000",
-    '9': "1001",
-    'A': "1010",
-    'B': "1011",
-    'C': "1100",
-    'D': "1101",
-    'E': "1110",
-    'F': "1111"
+code_table = {
+    (3, 2, 1, 1): 0,
+    (2, 2, 2, 1): 1,
+    (2, 1, 2, 2): 2,
+    (1, 4, 1, 1): 3,
+    (1, 1, 3, 2): 4,
+    (1, 2, 3, 1): 5,
+    (1, 1, 1, 4): 6,
+    (1, 3, 1, 2): 7,
+    (1, 2, 1, 3): 8,
+    (3, 1, 1, 2): 9,
 }
 
-code_dict = {
-    '3211': 0,
-    '2221': 1,
-    '2122': 2,
-    '1411': 3,
-    '1132': 4,
-    '1231': 5,
-    '1114': 6,
-    '1312': 7,
-    '1213': 8,
-    '3112': 9
-}
 
+def find_back(start):
+    for back_idx in range(start, 2, -1):
+        if bin_code[back_idx] == "1":
+            return back_idx
+    else:
+        return -1
+
+
+# main loop
 for t_c in range(1, T + 1):
     N, M = map(int, input().split())
-    code = [input().strip() for _ in range(N)]
-    tmp_str = ''
-    result = 0
-    for elem in code[find_start(0)]:
-        tmp_str += hex_dict[elem]
-    K = M * 4 - 1
-    for i in range(K, -1, -1):
-        if tmp_str[i] == '1':
-            break
-    mul, flag = 1, False
-    # print(i, start_j)
-    # print(tmp_str)
-    # print(tmp_str[start_j:start_j + 7])
-    start_j = i - 55
-    cnt_n = ''
-    while True:
-        cnt_idx = 0
-        cnt_lst = [0, 0, 0, 0]
-        pre_v = '0'
-        for k in range(mul * 7):
-            if tmp_str[start_j + k] == pre_v:
-                cnt_lst[cnt_idx] += 1
-            else:
-                cnt_idx += 1
-                if cnt_idx > 3: break
-                cnt_lst[cnt_idx] += 1
-            pre_v = tmp_str[start_j + k]
-        if min(cnt_lst):
-            for idx_cnt in range(4):
-                cnt_lst[idx_cnt] //= min(cnt_lst)
-        cnt_str = ''.join(map(str, cnt_lst))
-        if cnt_str in code_dict.keys():
-            break
-        if start_j - 55 * mul > 0:
-            start_j -= 55 * mul
-        else:
-            break
-        mul += 1
-    if 0 in cnt_lst:
-        result = 0
-    else:
-        result = 1
-    print(cnt_str)
-    # print(start_j)
-# pprint.pprint(code)
+    enc_code = [input().strip() for _ in range(N)]
+    res_set = set()
+    enc_set = set(enc_code)
+    # M 길이의 0을 제거
+    enc_set.remove("0" * M)
+    not_found = False
+    for item in enc_set:
+        mul = 1
+        bin_code = bin(int(item, 16))
+        back = find_back(len(bin_code) - 1)
+        while back >= 54:
+            dec_l = []
+            j = back
+            for k in range(8):
+                w2 = w3 = w4 = 0
+                while bin_code[j] == "1":
+                    w4 += 1
+                    j -= 1
+                while bin_code[j] == "0":
+                    w3 += 1
+                    j -= 1
+                while bin_code[j] == "1":
+                    w2 += 1
+                    j -= 1
+                w1 = 7 * mul - (w2 + w3 + w4)
+                dec_t = (w1 // mul, w2 // mul, w3 // mul, w4 // mul)
+                if k == 0 and code_table.get(dec_t, -1) == -1:
+                    not_found = True
+                    break
+                dec_l.append(code_table[dec_t])
+                j -= w1
+            if not_found:
+                mul += 1
+                not_found = False
+                continue
+            dec_l.reverse()
+            odd = dec_l[0] + dec_l[2] + dec_l[4] + dec_l[6]
+            even = dec_l[1] + dec_l[3] + dec_l[5]
+            if not (odd * 3 + even + dec_l[7]) % 10:
+                res_set.add("".join(map(str, dec_l)))
+            back -= 56 * mul - 1
+            mul = 1
+            back = find_back(back)
+    print(f"#{t_c} {sum(map(lambda x: sum(map(int, x)), res_set))}")
---
+++ 4366.py
@@ -0,0 +1,8 @@
+import sys
+
+
+sys.stdin = open('4366.txt.txt', 'r')
+
+T = int(input())
+for t_c in range(1, T + 1):
+    
