28dfc3b437f383bfa7cd5af6004ae97dc07ac7bc
devchoi <dev.sidamo@gmail.com>
2024-03-10 22:26:18 +0900

    add: 1251

--- 1251.py
+++ 1251.py
@@ -1,20 +1,32 @@
 import sys
-import math
 
 sys.stdin = open('1251.txt', 'r')
 
+
 T = int(input())
-# 지옥의 하나로마트
+
+def find(c):
+    if c != p[c]:
+        c = find(p[c])
+    return p[c]
+
+
+def union(a, b):
+    pa = find(a)
+    pb = find(b)
+    p[pb] = pa
+
 
 for t_c in range(1, T + 1):
     N = int(input())
-    X = list(map(float, input().split()))
-    Y = list(map(float, input().split()))
+    X = list(map(int, input().split()))
+    Y = list(map(int, input().split()))
     E = float(input())
-    MST = [0xfffffff] * N
-    for i in range(N - 1):
-        for j in range(i + 1, N):
-            dst = math.sqrt((X[i] - X[j]) ** 2 + (Y[i] - Y[j]) ** 2)
-            if dst < MST[i]:
-                MST[i] = dst
-    print(MST)
+    MST = sorted([[((X[i] - X[j]) ** 2 + (Y[i] - Y[j]) ** 2) ** 0.5, i, j] for i in range(N) for j in range(i + 1, N)])
+    p = [i for i in range(N)]
+    sum_dst = 0
+    for dst, start, end in MST:
+        if find(start) != find(end):
+            union(start, end)
+            sum_dst += dst ** 2
+    print(f'#{t_c} {int(round(E * sum_dst, 0))}')
